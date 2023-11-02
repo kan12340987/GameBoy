8fde4ef8fdabfa4ae701d23d2d82e4711a15d50b
kan12340987 <avinashnayak2002@gmail.com>
2023-11-02 23:28:59 +0530

    SDL window creattion

--- src/emu_core.cpp
+++ src/emu_core.cpp
@@ -1,5 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+//Using SDL and standard IO
+ #include <SDL2/SDL.h>
+ const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 int main() {
-    cout << "Hello" << endl;
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+    
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
+    //Get window surface
+    screenSurface = SDL_GetWindowSurface( window );
+
+    //Fill the surface white
+    SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            
+    //Update the surface
+    SDL_UpdateWindowSurface( window );
+
+    //Hack to get window to stay up
+    SDL_Event e;
+     bool quit = false; 
+     while( quit == false ){
+         while( SDL_PollEvent( &e ) )
+         { 
+            if( e.type == SDL_QUIT ) 
+            quit = true; 
+        } 
+    }
 }
