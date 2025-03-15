fb4fd4de3b17a8afa3d2d75a43427a5d015a2f16
kan12340987 <avinashnayak2002@gmail.com>
2025-03-15 13:19:03 +0530

    Added more decoding opcodes procedures

--- src/MMU.cpp
+++ src/MMU.cpp
@@ -9,8 +9,11 @@ __uint8_t MMU::readByte() {
     return ram[PC++];
 }
 char * MMU::readBootRom(char * filePath) {
-    ifstream file(filePath,ios::binary);
-    file.read((char *)ram, 0x100);
+    ifstream DMG_ROM(filePath,ios::binary);
+    DMG_ROM.seekg(0, std::ios::end);
+    long size = DMG_ROM.tellg();
+    DMG_ROM.seekg(0, std::ios::beg);
+    DMG_ROM.read((char *)ram, 0x100);
     return NULL;
 }
 char * MMU::readCartridge(char * filePath) {
